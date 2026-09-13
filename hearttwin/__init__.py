06c562b8ba4edea11a919e3bb0dd64d3ace4ac50
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-14 02:09:56 +0300

    feat: export cardiac twin primitives

--- hearttwin/__init__.py
+++ hearttwin/__init__.py
@@ -2,5 +2,16 @@ from .orchestrator import HeartTwin
 from .config import load_registry
 from .contracts import CardiacState, Observation, Provenance, ServiceResult, TwinRun
 from .api import VirelionServices
+from .cardiac_twin import (
+    CardiacDigitalTwin, CalibrationSpec, ConductionNetwork, ECGObservation,
+    EPParameters, MeshGeometry, PseudoECG, ScarMap, TwinSimulation,
+    UpstreamCardiacDigitalTwinAdapter,
+)
 
-__all__ = ["HeartTwin", "VirelionServices", "load_registry", "CardiacState", "Observation", "Provenance", "ServiceResult", "TwinRun"]
+__all__ = [
+    "HeartTwin", "VirelionServices", "load_registry", "CardiacState",
+    "Observation", "Provenance", "ServiceResult", "TwinRun",
+    "CardiacDigitalTwin", "CalibrationSpec", "ConductionNetwork",
+    "ECGObservation", "EPParameters", "MeshGeometry", "PseudoECG",
+    "ScarMap", "TwinSimulation", "UpstreamCardiacDigitalTwinAdapter",
+]
