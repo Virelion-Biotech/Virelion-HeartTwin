689bff80943392416bddedf7879d9b3b2c54978e
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-17 22:41:02 +0300

    feat: export canonical CardiacState APIs

--- hearttwin/__init__.py
+++ hearttwin/__init__.py
@@ -10,14 +10,21 @@ from .contracts import (
     LearningResultPayload,
     ModalityAnalysisPayload,
     Observation,
+    PredictionArtifact,
     Provenance,
     ServiceResult,
+    SimulationArtifact,
     SimulationResultPayload,
+    StateTransition,
+    StateValue,
     TwinRun,
+    UncertaintySpec,
+    ValidationArtifact,
     VexObservationPayload,
     WorkflowRun,
     WorkflowState,
 )
+from .state import CardiacStateStore, CardiacStateValidationError, state_from_service_results
 from .api import VirelionServices
 from .workflow import WorkflowError, run_multimodal_workflow
 from .cardiac_twin import (
@@ -29,9 +36,12 @@ from .cardiac_twin import (
 __all__ = [
     "HeartTwin", "VirelionServices", "load_registry",
     "CardiacState", "Observation", "Provenance", "ServiceResult", "TwinRun",
+    "StateValue", "UncertaintySpec", "PredictionArtifact", "SimulationArtifact",
+    "ValidationArtifact", "StateTransition",
     "AtlasContextPayload", "BenchmarkResolutionPayload", "LearningResultPayload",
     "SimulationResultPayload", "EvaluationResultPayload", "AgentChallengePayload",
     "VexObservationPayload", "ModalityAnalysisPayload", "BridgePublicationPayload",
+    "CardiacStateStore", "CardiacStateValidationError", "state_from_service_results",
     "WorkflowState", "WorkflowRun", "WorkflowError", "run_multimodal_workflow",
     "CardiacDigitalTwin", "CalibrationSpec", "ConductionNetwork",
     "ECGObservation", "EPParameters", "MeshGeometry", "PseudoECG",
