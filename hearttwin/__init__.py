b1ebaa1220a391029ab5386aa7ec06bb50fe3191
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-17 22:22:08 +0300

    feat: expose native services and multimodal workflow

--- hearttwin/__init__.py
+++ hearttwin/__init__.py
@@ -1,7 +1,23 @@
 from .orchestrator import HeartTwin
 from .config import load_registry
-from .contracts import CardiacState, Observation, Provenance, ServiceResult, TwinRun
+from .contracts import (
+    AgentChallengePayload,
+    AtlasContextPayload,
+    BenchmarkResolutionPayload,
+    BridgePublicationPayload,
+    CardiacState,
+    EvaluationResultPayload,
+    LearningResultPayload,
+    Observation,
+    Provenance,
+    ServiceResult,
+    SimulationResultPayload,
+    TwinRun,
+    WorkflowRun,
+    WorkflowState,
+)
 from .api import VirelionServices
+from .workflow import WorkflowError, run_multimodal_workflow
 from .cardiac_twin import (
     CardiacDigitalTwin, CalibrationSpec, ConductionNetwork, ECGObservation,
     EPParameters, MeshGeometry, PseudoECG, ScarMap, TwinSimulation,
@@ -9,8 +25,12 @@ from .cardiac_twin import (
 )
 
 __all__ = [
-    "HeartTwin", "VirelionServices", "load_registry", "CardiacState",
-    "Observation", "Provenance", "ServiceResult", "TwinRun",
+    "HeartTwin", "VirelionServices", "load_registry",
+    "CardiacState", "Observation", "Provenance", "ServiceResult", "TwinRun",
+    "AtlasContextPayload", "BenchmarkResolutionPayload", "LearningResultPayload",
+    "SimulationResultPayload", "EvaluationResultPayload", "AgentChallengePayload",
+    "BridgePublicationPayload", "WorkflowState", "WorkflowRun", "WorkflowError",
+    "run_multimodal_workflow",
     "CardiacDigitalTwin", "CalibrationSpec", "ConductionNetwork",
     "ECGObservation", "EPParameters", "MeshGeometry", "PseudoECG",
     "ScarMap", "TwinSimulation", "UpstreamCardiacDigitalTwinAdapter",
